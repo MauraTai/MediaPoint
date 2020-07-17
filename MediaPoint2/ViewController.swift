d7d6f68f1cc80985b8ac7438f81dba41ae97c043
MauraTai <mauratjenkins@yahoo.com>
2020-07-17 16:09:10 -0700

    Rework of Base Project

---
+++ MediaPoint2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaPoint2
+//
+//  Created by Maura Tai on 7/17/20.
+//  Copyright © 2020 Maura Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '13.0'
+
+target 'MediaPoint2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MediaPoint2
+
+pod 'Firebase/Analytics'
+pod 'Firebase/Storage'
+pod 'Firebase/Database'
+
+end
